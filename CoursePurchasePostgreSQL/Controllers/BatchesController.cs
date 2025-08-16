64cd11b36ab33c882bf98bff671d00dfd46b6fc2
Anjana P <anjanakrishnadas3@gmail.com>
2025-08-16 22:27:17 +0530

    Add project files.

---
+++ CoursePurchasePostgreSQL/Controllers/BatchesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CoursePurchasePostgreSQL.Models;
+
+namespace CoursePurchasePostgreSQL.Controllers
+{
+    public class BatchesController : Controller
+    {
+        private readonly CoursePurchaseContext _context;
+
+        public BatchesController(CoursePurchaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Batches
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Batches.ToListAsync());
+        }
+
+        // GET: Batches/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batch = await _context.Batches
+                .FirstOrDefaultAsync(m => m.BatchId == id);
+            if (batch == null)
+            {
+                return NotFound();
+            }
+
+            return View(batch);
+        }
+
+        // GET: Batches/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Batches/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BatchId,BatchName,StartDate,TentativeEndDate,EndDate,Fees,FeesPaid,Duration,HoursTaken,Status,Details,Remarks,BatchImageUrl")] Batch batch)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(batch);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(batch);
+        }
+
+        // GET: Batches/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batch = await _context.Batches.FindAsync(id);
+            if (batch == null)
+            {
+                return NotFound();
+            }
+            return View(batch);
+        }
+
+        // POST: Batches/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BatchId,BatchName,StartDate,TentativeEndDate,EndDate,Fees,FeesPaid,Duration,HoursTaken,Status,Details,Remarks,BatchImageUrl")] Batch batch)
+        {
+            if (id != batch.BatchId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(batch);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BatchExists(batch.BatchId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(batch);
+        }
+
+        // GET: Batches/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batch = await _context.Batches
+                .FirstOrDefaultAsync(m => m.BatchId == id);
+            if (batch == null)
+            {
+                return NotFound();
+            }
+
+            return View(batch);
+        }
+
+        // POST: Batches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var batch = await _context.Batches.FindAsync(id);
+            if (batch != null)
+            {
+                _context.Batches.Remove(batch);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BatchExists(int id)
+        {
+            return _context.Batches.Any(e => e.BatchId == id);
+        }
+    }
+}
