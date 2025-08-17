c2e4b7ff442181dcbd41fe4c1bc0dd0b148dfc07
Liya Latheef <liyalatheef23@gmail.com>
2025-08-17 16:12:39 +0530

    Added navigatin link to Batches

--- CoursePurchasePostgreSQL/Models/CoursePurchaseContext.cs
+++ CoursePurchasePostgreSQL/Models/CoursePurchaseContext.cs
@@ -27,7 +27,7 @@ public partial class CoursePurchaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=CoursePurchase;Username=postgres;Password=12378900");
+        => optionsBuilder.UseNpgsql("Host=localhost;Password=admin;Username=postgres;Database=CoursePurchase");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
